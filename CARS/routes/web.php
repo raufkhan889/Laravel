3db1220b441d7fbeb6f24173da908d376fa7d3a3
Rauf Khan <khanrauf578@gmail.com>
2021-11-14 11:08:34 +0500

    modified

--- CARS/routes/web.php
+++ CARS/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CarsController;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\CarModelsController;
